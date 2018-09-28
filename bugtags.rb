37e33c070b91eaad569fb39512365d5752954ec2
wliu6 <1029769711@qq.com>
2018-09-28 19:50:43 +0800

    optimized

--- bugtags.rb
+++ bugtags.rb
@@ -65,19 +65,31 @@ module Fastlane
           FastlaneCore::ConfigItem.new(key: :app_version,
                                        env_name: "FL_BUGTAGS_APP_VERSION",
                                        description: "the version in your project's info.plist",
-                                       ),
+                                       verify_block: proc do |value|
+                                          UI.user_error!("No version for BugtagsAction given, pass using `dsym: 'app_version file's path'`") unless (value and not value.empty?)
+                                          # UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                       end),
           FastlaneCore::ConfigItem.new(key: :app_build,
                                        env_name: "FL_BUGTAGS_APP_BUILD",
                                        description: "the build version in your project's info.plist",
-                                       ),
+                                       verify_block: proc do |value|
+                                          UI.user_error!("No build version for BugtagsAction given, pass using `app_build: 'build version'`") unless (value and not value.empty?)
+                                          # UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                       end),
           FastlaneCore::ConfigItem.new(key: :app_key,
                                        env_name: "FL_BUGTAGS_APP_KEY",
                                        description: "Bugtags App ID key e.g. 29f35c87cb99e10e00c7xxxx",
-                                       ),
+                                       verify_block: proc do |value|
+                                          UI.user_error!("No App ID for BugtagsAction given, pass using `app_key: 'Bugtags App ID key'`") unless (value and not value.empty?)
+                                          # UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                       end),
           FastlaneCore::ConfigItem.new(key: :secret_key,
                                        env_name: "FL_BUGTAGS_SECRET_KEY",
                                        description: "Bugtags App Secret key e.g. 29f35c87cb99e10e00c7xxxx",
-                                       )
+                                       verify_block: proc do |value|
+                                          UI.user_error!("No App Secret for BugtagsAction given, pass using `secret_key: 'Bugtags App Secret key'`") unless (value and not value.empty?)
+                                          # UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                       end)
                                        #is_string: false, # true: verifies the input is a string, false: every kind of value
                                        #default_value: false) # the default value if the user didn't provide one
         ]
